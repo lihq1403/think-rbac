db69a9827d8a0f1b51486276051313c560b2b636
lihq <873344138@qq.com>
2019-06-27 15:07:17 +0800

    配置文件更新

--- src/config/rbac.php
+++ src/config/rbac.php
@@ -11,5 +11,14 @@ return [
     /**
      * 是否忽略未定义的权限
      */
-    'skip_undefined_permission' => true
+    'skip_undefined_permission' => true,
+
+    /**
+     * 可以跳过权限验证的方法
+     */
+    'continue_list' => [
+        'module' => ['admin'],
+        'controller' => ['test'],
+        'action' => ['test']
+    ],
 ];
